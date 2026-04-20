eb6d579c31deb97ae9b3d591d571cc8213f6855a
afamiliarquiet <afamiliarquiet@gmail.com>
2026-04-20 00:12:31 -0700

    tags and recipes
    i don't like datagen. not gonna use it for tags. for recipes and their advancements however.. it's still not even that much of a help! but it's a little help. i'll take it.

--- src/client/java/ink/quietly/archery_gimmicks/client/ArcheryGimmicksDataGenerator.java
+++ src/client/java/ink/quietly/archery_gimmicks/client/ArcheryGimmicksDataGenerator.java
@@ -5,25 +5,34 @@ import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.client.data.models.model.ItemModelUtils;
 import net.minecraft.client.data.models.model.ModelTemplates;
 import net.minecraft.client.renderer.item.ItemModel;
 import net.minecraft.client.renderer.item.properties.numeric.UseDuration;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import org.jspecify.annotations.NonNull;
+
+import java.util.concurrent.CompletableFuture;
 
 public class ArcheryGimmicksDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(@NonNull FabricDataGenerator fabricDataGenerator) {
 		var pack = fabricDataGenerator.createPack();
-		pack.addProvider(ModelProvider::new);
+		pack.addProvider(GimmickModelProvider::new);
+		pack.addProvider(GimmickRecipeProvider::new);
 	}
 
-	public static class ModelProvider extends FabricModelProvider {
+	public static class GimmickModelProvider extends FabricModelProvider {
 
-		public ModelProvider(FabricPackOutput output) {
+		public GimmickModelProvider(FabricPackOutput output) {
 			super(output);
 		}
 
@@ -56,6 +65,75 @@ public class ArcheryGimmicksDataGenerator implements DataGeneratorEntrypoint {
 						bowModel
 					)
 				);
+		}
+	}
+
+	public static class GimmickRecipeProvider extends FabricRecipeProvider {
+
+		public GimmickRecipeProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+			super(output, registriesFuture);
+		}
+
+		@Override
+		protected @NonNull RecipeProvider createRecipeProvider(HolderLookup.@NonNull Provider registries, @NonNull RecipeOutput output) {
+			return new RecipeProvider(registries, output) {
+				@Override
+				public void buildRecipes() {
+					// riddle me this, docs.fabricmc.net..
+//					HolderLookup.RegistryLookup<Item> itemLookup = registries.lookupOrThrow(Registries.ITEM);
+
+					shaped(RecipeCategory.COMBAT, ItemBag.LIGHT_BOW)
+						.pattern(" *s")
+						.pattern("/ s")
+						.pattern(" *s")
+						.define('*', Items.GLOWSTONE_DUST)
+						.define('s', Items.STRING)
+						.define('/', Items.STICK)
+						.unlockedBy(getHasName(ItemBag.LIGHT_BOW), has(ItemBag.LIGHT_BOW))
+						.unlockedBy(getHasName(Items.BOW), has(Items.BOW))
+						.unlockedBy(getHasName(Items.GLOWSTONE_DUST), has(Items.GLOWSTONE_DUST))
+						.save(output);
+					shaped(RecipeCategory.COMBAT, ItemBag.HEAVY_BOW)
+						.pattern(" /s")
+						.pattern("# s")
+						.pattern(" /s")
+						.define('#', Items.NETHERITE_SCRAP)
+						.define('s', Items.STRING)
+						.define('/', Items.STICK)
+						.unlockedBy(getHasName(ItemBag.HEAVY_BOW), has(ItemBag.HEAVY_BOW))
+						.unlockedBy(getHasName(Items.BOW), has(Items.BOW))
+						.unlockedBy(getHasName(Items.NETHERITE_SCRAP), has(Items.NETHERITE_SCRAP))
+						.save(output);
+
+					shaped(RecipeCategory.COMBAT, ItemBag.WEIGHTED_ARROW, 4)
+						.pattern(".")
+						.pattern("|")
+						.pattern("#")
+						.define('.', Items.IRON_NUGGET)
+						.define('|', Items.IRON_INGOT)
+						.define('#', Items.FEATHER) // you're gonna have to put in a lot of work, little guy. good luck
+						.unlockedBy(getHasName(ItemBag.WEIGHTED_ARROW), has(ItemBag.WEIGHTED_ARROW))
+						.unlockedBy(getHasName(Items.ARROW), has(Items.ARROW))
+						.unlockedBy(getHasName(Items.IRON_INGOT), has(Items.IRON_INGOT))
+						.save(output);
+					shaped(RecipeCategory.COMBAT, ItemBag.SIGNAL_ARROW, 4)
+						.pattern("c")
+						.pattern("/")
+						.pattern("#")
+						.define('c', Items.COAL) // fight me, torch placing arrow mods
+						.define('/', Items.STICK)
+						.define('#', Items.FEATHER) // you're gonna have to put in a lot of work, little guy. good luck
+						.unlockedBy(getHasName(ItemBag.SIGNAL_ARROW), has(ItemBag.SIGNAL_ARROW))
+						.unlockedBy(getHasName(Items.ARROW), has(Items.ARROW))
+						.unlockedBy(getHasName(Items.TORCH), has(Items.TORCH))
+						.save(output);
+				}
+			};
+		}
+
+		@Override
+		public @NonNull String getName() {
+			return "Recipe Definitions";
 		}
 	}
 }
